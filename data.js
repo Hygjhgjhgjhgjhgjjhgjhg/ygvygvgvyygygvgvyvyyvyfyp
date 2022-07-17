8b2619a74f41e841752c92b4f0e54a853c815e03
Hygjhgjhgjhgjhgjjhgjhg <97777899+Hygjhgjhgjhgjhgjjhgjhg@users.noreply.github.com>
2022-07-17 15:55:00 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,6 @@
-var IDS = ['007']
+var IDS = ['007', '008', '009']
 var data = {
   '007' : 'https://github.com/Hygjhgjhgjhgjhgjjhgjhg/ygvygvgvyygygvgvyvyyvyfyp/raw/main/server/007.png',
+  '008' : 'https://github.com/Hygjhgjhgjhgjhgjjhgjhg/ygvygvgvyygygvgvyvyyvyfyp/raw/main/server/008.png',
+  '009' ; 'https://github.com/Hygjhgjhgjhgjhgjjhgjhg/ygvygvgvyygygvgvyvyyvyfyp/raw/main/server/Screenshot_2022-07-17-15-51-35-051.jpeg',
   }
