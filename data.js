de583b2c128c7dc1c78fc7034b35c6f9308adb15
Hygjhgjhgjhgjhgjjhgjhg <97777899+Hygjhgjhgjhgjhgjjhgjhg@users.noreply.github.com>
2022-07-17 13:11:45 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
 var IDS = ['007']
 var data = {
-  '007' : 'https://github.com/Hygjhgjhgjhgjhgjjhgjhg/ygvygvgvyygygvgvyvyyvyfyp/blob/main/server/007.png',
+  '007' : 'https://github.com/Hygjhgjhgjhgjhgjjhgjhg/ygvygvgvyygygvgvyvyyvyfyp/raw/main/server/007.png',
   }
